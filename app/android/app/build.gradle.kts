8f7960ad0586dd09aebbbbd8b8e0ff9a5ad216f4
kaliktg <86680782+kaliktg@users.noreply.github.com>
2026-06-04 15:06:43 +0300

    Clarify Android release signing scaffold

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "dev.babelfish.babel_fish_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Local release smoke builds use debug signing. Distribution signing
+            // must be configured outside git in a reviewed release pipeline.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
